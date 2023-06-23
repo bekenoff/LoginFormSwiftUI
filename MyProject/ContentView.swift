e5769d977cd587a681125a25799ad0f0bd75d7b9
bekenov <nursultan.bekenov1@gmail.com>
2023-06-23 19:49:34 +0600

    Initial Commit

---
+++ MyProject/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  MyProject
+//
+//  Created by Nursultan on 07.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var colors = ["red", "white", "blue"]
+    
+    @State private var click = false
+    @State private var selectedColor = 0
+    @State private var tggl = false
+    @State private var username = ""
+    @State private var password = ""
+    
+    var body: some View {
+   
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.4)
+                    .foregroundColor(.white).opacity(0.15)
+                
+                Circle()
+                    .scale(1.1)
+                    .foregroundColor(.white)
+                
+                
+                    .navigationBarHidden(true)
+                
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    
+                    TextField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        
+                    Button("Login") {
+                        
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding(.top)
+                    
+                    Button("Do you have an account?") {
+                        
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by Nursultan on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
