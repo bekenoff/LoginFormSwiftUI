bed798623ccbe2b7fd91a5c4fff597f6cca05f26
bekenoff <98037992+bekenoff@users.noreply.github.com>
2023-11-16 20:43:31 +0600

    Update ContentView.swift

--- MyProject/ContentView.swift
+++ MyProject/ContentView.swift
@@ -1,25 +1,22 @@
-//
-//  ContentView.swift
-//  MyProject
-//
-//  Created by Nursultan on 07.06.2023.
-//
-
 import SwiftUI
 
+// User struct conforms to Codable to be encoded to JSON
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+    var email: String
+    var phoneNumber: String
+    var password: String
+}
+
 struct ContentView: View {
-    
-    var colors = ["red", "white", "blue"]
-    
-    @State private var click = false
-    @State private var selectedColor = 0
-    @State private var tggl = false
-    @State private var username = ""
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var phoneNumber = ""
     @State private var password = ""
-    @State private var number = ""
-    
+
     var body: some View {
-        
         NavigationView {
             ZStack {
                 Color.blue
@@ -27,83 +24,94 @@ struct ContentView: View {
                 Circle()
                     .scale(1.8)
                     .foregroundColor(.white).opacity(0.15)
-                
                 Circle()
                     .scale(1.5)
                     .foregroundColor(.white)
-                
-                
                     .navigationBarHidden(true)
-                
+
                 VStack {
                     Text("Registration")
                         .font(.largeTitle)
                         .bold()
                         .padding()
-                    
-                    
-                    TextField("Email", text: $username)
+
+                    // Your text fields for input
+                    TextField("First name", text: $firstName)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background(Color.black.opacity(0.05))
                         .cornerRadius(10)
-                    
-                    TextField("First name", text: $username)
+                    TextField("Last name", text: $lastName)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background(Color.black.opacity(0.05))
                         .cornerRadius(10)
-                    TextField("Last name", text: $username)
+                    TextField("Email", text: $email)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background(Color.black.opacity(0.05))
                         .cornerRadius(10)
-                    
-                    TextField("Password", text: $password)
+                    TextField("Phone Number", text: $phoneNumber)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background(Color.black.opacity(0.05))
                         .cornerRadius(10)
-                    
-                    TextField("Number", text: $number)
+                    SecureField("Password", text: $password)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background(Color.black.opacity(0.05))
                         .cornerRadius(10)
-                    
-                    
-                    
+
+                    // Registration button
                     Button("Registration") {
-                        
+                        let user = User(firstName: firstName,
+                                        lastName: lastName,
+                                        email: email,
+                                        phoneNumber: phoneNumber,
+                                        password: password)
+                        if let userData = try? JSONEncoder().encode(user) {
+                            postUser(userData: userData)
+                        }
                     }
                     .foregroundColor(.white)
                     .frame(width: 300, height: 50)
                     .background(Color.blue)
                     .cornerRadius(10)
                     .padding(.top)
-                    
-                    
-                    
-                        
-                        
-                    Button(action: {
-                        print("Floating Button Click")
-                    }, label: {
-                        NavigationLink(destination: ContentView()) {
-                             Text("Do you have an account?")
-                         }
-                    })
-                    
                 }
             }
         }
     }
-    
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+
+    // Function to post user data
+    func postUser(userData: Data) {
+        guard let url = URL(string: "http://localhost:1000/api/register") else {
+            // Handle the case when the URL is invalid
+            return
         }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = userData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedUser = try? JSONDecoder().decode(User.self, from: data) {
+                    // Handle the response, you can access decodedUser here
+                    print(decodedUser)
+                }
+            } else if let error = error {
+                // Handle the error
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
